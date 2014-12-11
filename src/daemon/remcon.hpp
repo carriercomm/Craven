05295257dcc55f7258be908a6f470cc03a002d41
Tom Johnson <public@tomjohnson.me.uk>
2014-12-11 21:46:39 +0000

    Added documentation for detail::connection.

--- src/daemon/remcon.hpp
+++ src/daemon/remcon.hpp
@@ -4,24 +4,70 @@
 
 namespace detail
 {
+	//! Connection handler
+	/*!
+	 *  This class handles a connection to a client from the server. It can
+	 *  only be created as a std::shared_ptr -- it's a shared object. When it
+	 *  destructs, it closes the socket it owns, cleaning up the connection
+	 *  automatically.
+	 */
 	class connection : public std::enable_shared_from_this<connection>
 	{
+		//! Construct the connection.
+		/*!
+		 *  This function is only accessable within the class, so we cannot
+		 *  make instances of this class outside its members. See create() for
+		 *  how instances can be publicly created.
+		 *  \param socket A reference to the socket for which this class is
+		 *  built around. This socket is moved into the class.
+		 */
 		connection(boost::asio::local::stream_protocol::socket& socket);
 	public:
+		//! The pointer type for this class.
 		typedef std::shared_ptr<connection> pointer;
 
+		//! Create a new instance of the class from an io_service.
+		/*!
+		 *  This is one of two overloads of this function, the only function
+		 *  able to create new instances of this class.
+		 *
+		 *  It creates a new class (with a new socket) from the given
+		 *  io_service, wrapping it in a shared_ptr and returning it.
+		 *  \param io_service The io_service with which the connection's socket
+		 *  should be registered.
+		 *  \returns A new instance of the class, wrapped in a std::shared_ptr
+		 */
 		static pointer create(boost::asio::io_service& io_service);
 
+		//
 		template <class T>
 		static pointer create(T&& socket)
 		{
 			return std::make_shared<connection>(std::forward(socket));
 		}
 
+		//! Return the socket managed by this class.
+		//! \return The socket managed by this class.
 		boost::asio::local::stream_protocol::socket& socket();
 
+		//! Add a message to the write queue.
+		/*!
+		 *  This function adds a message to the queue to be asynchronously sent
+		 *  when the socket is available for writing. Thread safe.
+		 *
+		 *  \param msg The message to add to the message queue.
+		 */
 		void queue_write(const std::string& msg);
 
+		//! Register a callback for a read event.
+		/*!
+		 *  This function registers a callback to receive the strings read
+		 *  asynchronously from the managed socket.
+		 *  \param f The callable object to call when a string is received --
+		 *  must be able to take a string.
+		 *
+		 *  \returns An object that can be used for deregistering the callback.
+		 */
 		template <class Callable>
 		boost::signals2::connection connect_read(Callable&& f)
 		{
@@ -30,13 +76,19 @@ namespace detail
 
 
 	protected:
+		//! The socket managed by this class.
 		boost::asio::local::stream_protocol::socket socket_;
 
+		//! The message queue to be placed on the socket.
 		std::deque<std::string> write_queue_;
 
+		//! Handles the read callbacks.
 		boost::signals2::signal<void (const std::string&)> read_handlers_;
 
+		//! Callback for handling reads on the socket.
 		void handle_read(boost::system::error_code& error, std::size_t bytes_tx);
+
+		//! Callback for handling writes on the socket.
 		void handle_write(boost::system::error_code& error);
 	};
 
