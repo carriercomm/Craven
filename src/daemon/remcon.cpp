bcb501f794e27fe884e6ea28f4462d490cbfffe8
Tom Johnson <public@tomjohnson.me.uk>
2014-12-12 19:23:42 +0000

    Fixing syntax errors.

--- src/daemon/remcon.cpp
+++ src/daemon/remcon.cpp
@@ -6,7 +6,8 @@ namespace fs = boost::filesystem;
 
 #include "remcon.hpp"
 
-detail::connection::connection(boost::asio::local::stream_protocol::socket& socket)
+detail::connection::connection(boost::asio::local::stream_protocol::socket socket)
+	:socket_(std::move(socket))
 {
 	throw std::runtime_error("Not yet implemented");
 }
@@ -17,21 +18,20 @@ detail::connection::pointer detail::connection::create(boost::asio::io_service& 
 	return nullptr;
 }
 
-boost::asio::local::stream_protocol::socket& detail::connection:socket()
-{
-	return socket_
-};
-
-const boost::asio::local::stream_protocol::socket& detail::connection:socket() const
+boost::asio::local::stream_protocol::socket& detail::connection::socket()
 {
 	return socket_;
 }
 
-void detail::connection:queue_write(const std::string& msg)
+const boost::asio::local::stream_protocol::socket& detail::connection::socket() const
+{
+	return socket_;
+}
+
+void detail::connection::queue_write(const std::string& msg)
 {
 	throw std::runtime_error("Not yet implemented");
 }
-
 
 
 RemoteControl::RemoteControl(boost::asio::io_service& io, const fs::path& socket)
