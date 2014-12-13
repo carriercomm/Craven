8189dcfe13398cb673f9fb7e30543aca5388d5d8
Tom Johnson <public@tomjohnson.me.uk>
2014-12-13 21:23:44 +0000

    Added connection test file.

---
+++ src/common/test/connection-test.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE "Utility"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(single_handler_trait)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(multiple_handler_trait)
+{
+
+}
