cc2764411a4cf93af8dc76f8335cf5a9826c1701
Tom Johnson <public@tomjohnson.me.uk>
2014-12-15 17:14:55 +0000

    Added (failing) test file for ConnectionPool

---
+++ src/daemon/test/connection_pool-test.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE "Connection pool test"
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+#include "../connection_pool.hpp"
+
+BOOST_AUTO_TEST_CASE(fnord)
+{
+
+	BOOST_REQUIRE(false);
+}
