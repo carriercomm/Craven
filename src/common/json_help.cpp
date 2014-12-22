5bd1472d2c266d4f522b39eb9adf208b51e37115
Tom Johnson <public@tomjohnson.me.uk>
2014-12-22 15:55:33 +0000

    Adding JSON helpers' source and header files.

---
+++ src/common/json_help.cpp
@@ -0,0 +1,100 @@
+#include <cstdint>
+
+#include <string>
+#include <vector>
+#include <iterator>
+
+#include <json/json.h>
+
+#include "json_help.hpp"
+
+Json::Value json_help::parse(const std::string& line)
+{
+	Json::Reader r;
+	Json::Value value;
+	r.parse(line, value);
+	return value;
+}
+
+bool json_help::parse(const std::string& line, Json::Value& root)
+{
+	Json::Reader r;
+	return r.parse(line, root);
+}
+
+std::string json_help::write(const Json::Value& root)
+{
+	Json::FastWriter w;
+	return w.write(root);
+}
+
+
+void membership_check(const Json::Value& root, const std::string& key, const std::string& msg)
+{
+	if(!root.isMember(key))
+		throw std::runtime_error(msg + " no such member \"" + key + "\"");
+}
+
+template<>
+uint32_t json_help::checked_from_json<uint32_t>(const Json::Value& root, const std::string& key, const std::string& msg)
+{
+	membership_check(root, key, msg);
+
+	if(!root[key].isInt() || !root[key].asInt() >= 0)
+		throw std::runtime_error(msg + " " + key + " is not a uint");
+
+	return static_cast<uint32_t>(root[key].asInt());
+}
+
+template<>
+std::string json_help::checked_from_json<std::string>(const  Json::Value& root, const std::string& key, const std::string& msg)
+{
+	membership_check(root, key, msg);
+
+	if(!root[key].isString())
+		throw std::runtime_error(msg + " " + key + " is not a string");
+
+	return root[key].asString();
+}
+
+template<>
+std::vector<std::string> json_help::checked_from_json<std::vector<std::string>>(const
+		Json::Value& root, const std::string& key, const std::string& msg)
+{
+	membership_check(root, key, msg);
+
+	if(!root[key].isArray())
+		throw std::runtime_error(msg + " " + key + " is not an array");
+
+	std::vector<std::string> value;
+	value.reserve(root[key].size());
+
+	for(const Json::Value& val : root[key])
+	{
+		if(!val.isString())
+			throw std::runtime_error(msg + " " + key + " element is not a string");
+
+		value.push_back(val.asString());
+	}
+
+	return value;
+}
+
+
+template<>
+std::vector<Json::Value> json_help::checked_from_json<std::vector<Json::Value>>(const
+		Json::Value& root, const std::string& key, const std::string& msg)
+{
+	membership_check(root, key, msg);
+
+	if(!root[key].isArray())
+		throw std::runtime_error(msg + " " + key + " is not an array");
+
+	std::vector<Json::Value> value;
+	value.reserve(root[key].size());
+
+	for(const Json::Value& val : root[key])
+		value.push_back(val);
+
+	return value;
+}
---
+++ src/common/json_help.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+//! Helper functions for Json deserialisation
+namespace json_help
+{
+	Json::Value parse(const std::string& line);
+	bool parse(const std::string& line, Json::Value& root);
+
+	std::string write(const Json::Value& root);
+
+	template <typename T>
+	T checked_from_json(const Json::Value& root, const std::string& key, const std::string& msg);
+
+	template<>
+	uint32_t checked_from_json<uint32_t>(const Json::Value& root, const std::string& key, const std::string& msg);
+
+	template<>
+	std::string checked_from_json<std::string>(const Json::Value& root, const std::string& key, const std::string& msg);
+
+	template<>
+	std::vector<std::string> checked_from_json<std::vector<std::string>>(const Json::Value& root, const std::string& key, const std::string& msg);
+
+	template<>
+	std::vector<Json::Value> checked_from_json<std::vector<Json::Value>>(const Json::Value& root, const std::string& key, const std::string& msg);
+}
